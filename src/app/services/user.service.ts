adc32fae88e67c1b48e23aa941f1f16a31494021
Eric Bielmann <ericbielmann@gmail.com>
2018-10-02 00:58:31 -0300

    correct import

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { User } from '../models/User';
+import { User } from '../models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
